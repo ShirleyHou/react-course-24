3c2c7d5179737a99c3b1ef109806ab93ecf4bd4a
shirleyH-personal <xhou1128@gmail.com>
2024-12-30 03:14:11 +1100

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+
+
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+import HomePage from './pages/HomePage'
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout/>}>
+    <Route index element={<HomePage/>}/>
+    <Route path='jobs' element={<JobsPage/>}/>
+  </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-    <Navbar/>
-    <Hero title='title' subtitle='subtitle'/> 
-    <HomeCards/>
-    <JobListings/>
-    <ViewAllJobs/>
-    </>
-  )
-}
+  return (<RouterProvider router = {router}/>
+  );
+};
 
-export default App
+export default App;
